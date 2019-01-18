1ffc37555dc9a5c791f1474062762317ccb138e4
luckyLucas - Main <lucas.granberg00@gmail.com>
2019-01-18 09:51:04 +0100

    Added Unity project

---
+++ Catapult_ML-Agent/Assets/CollisionDetect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetect : MonoBehaviour
+{
+    public AlienAgent agent;
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.CompareTag("Alien"))
+        {
+            agent.CoinCollected();
+        }
+    }
+}
